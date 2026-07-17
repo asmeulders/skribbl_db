e8682211ef6c871e9d7ebd52159fd690529b8472
asmeulders <alexs23@bu.edu>
2026-07-17 13:49:29 -0400

    added WordSet model

--- skribbl_db/words/models.py
+++ skribbl_db/words/models.py
@@ -5,5 +5,21 @@ class Word(models.Model):
     word = models.CharField(max_length=32)
     active = models.BooleanField()
 
+    class Meta:
+        ordering = ["word"]
+
     def __str__(self):
         return f"{self.word}"
+    
+
+class WordSet(models.Model):
+    name = models.CharField(max_length=24, unique=True)
+    num_chars = models.PositiveIntegerField()
+    num_words = models.PositiveIntegerField()
+    words = models.ManyToManyField(Word)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} ({self.num_words} words)"
