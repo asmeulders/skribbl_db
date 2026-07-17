75948c828f50f20d196be21a06d80a9fd882992c
asmeulders <alexs23@bu.edu>
2026-07-17 14:37:34 -0400

    dynamically load added words to list

--- skribbl_db/words/models.py
+++ skribbl_db/words/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Word(models.Model):
     word = models.CharField(max_length=32)
-    active = models.BooleanField()
+    active = models.BooleanField(default=True)
 
     class Meta:
         ordering = ["word"]
--- skribbl_db/words/views.py
+++ skribbl_db/words/views.py
@@ -1,14 +1,28 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
 
 from .models import Word
 from .forms import NewWordForm
 
 def words(request):
+    if request.method == 'POST':
+        form = NewWordForm(request.POST)
+        if form.is_valid():
+            form.save()
+            all_words = Word.objects.all().values()
+            list_html = loader.render_to_string('_word_list.html', {'all_words': all_words}, request=request)
+            return JsonResponse({'success': True, 'list_html': list_html})
+        else:
+            return JsonResponse({'success': False, 'errors': form.errors}, status=400)
+    
+    else:
+        form = NewWordForm(initial={'word': ''})
+    
     all_words = Word.objects.all().values()
     template = loader.get_template('all_words.html')
     context = {
-        'all_words': all_words
+        'all_words': all_words,
+        'form': form
     }
     return HttpResponse(template.render(context, request))
 
